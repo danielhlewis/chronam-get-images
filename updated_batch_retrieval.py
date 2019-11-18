35e43dcf6de6e41ebed2aa3aef781ea19212670b
bcglee <bcgl@cs.washington.edu>
2019-11-17 18:08:20 -0800

    working on updating getImages

--- updated_batch_retrieval.py
+++ updated_batch_retrieval.py
@@ -4,6 +4,7 @@ import os
 import sys
 import requests
 import urllib
+import shutil
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 
@@ -57,8 +58,10 @@ def buildFullManifest():
         sys.stdout.write("\rProcessing "+str(count)+"/"+str(length)+" batch manifests (current: "+j+")")
         sys.stdout.flush()
 
+        # header for request
         headers = {'User-Agent':'Mozilla/5.0'}
 
+        # we open each manifest-md5.txt file to extract the filepaths of the .jp2 images (each image is a scan of a page)
         try:
             # sets path for "manifest-md5.txt"
             path = batchesURL+j+"manifest-md5.txt"
@@ -66,61 +69,122 @@ def buildFullManifest():
             page = requests.get(path)
             soup = BeautifulSoup(page.text, "html.parser")
 
-            # split lines
-            lines = str(soup).splitlines()
-
+            # creates list for storing image paths
             fullDataPaths = []
 
-            # then parse each line
+            # split lines in .txt
+            lines = str(soup).splitlines()
+
+            # then parse each line (we want to grab each image filepath that is in .jp2 format)
             for line in lines:
                 partialDataPath = line.split()
-                print(partialDataPath)
                 if partialDataPath[1].endswith('.jp2') and partialDataPath[1].count('/') == 4:
-                    fullDataPaths.append(j+partialDataPath[1]+'\n')
+                    fullDataPaths.append(batchesURL+j+partialDataPath[1]+'\n')
                     fullDataPaths.sort()
 
-        except:
-            print("PROBLEM")
+            openf.writelines(fullDataPaths)
 
-        # try to open sha1 manifest file, but if it isn't there, then try md5 file instead
-        try:
-            socket = urllib2.urlopen(batchesurl+j+"manifest-md5.txt")
-            print(socket)
-            sys.exit()
-        except:
-            print("HELP")
+        except Exception as e:
+            print("Encountered an error with batch " + j + " : "+ str(e) + "\n")
+            log.write("Batch " + j + " failed: " + str(e) + "\n")
 
-        # except err:
-        #     if err.code == 404 or err.code == 503:
-        #         try:
-        #             socket = urllib2.urlopen(j+"manifest-md5.txt")
-        #         except e:
-        #             print("Error HTTP: "+ str(e.code))
-        #             print("This is bad the manifest for batch "+j+" could not be acquired!!!\n")
-        #             log.write("Batch " + j + " failed: " + str(e.code) + "\n")
-        #     else:
-        #         print("Encountered an error when accessing the url: " + str(err.code) + "\n")
-        #         log.write("Batch " + j + "failed: " + str(err.code) + "\n")
-        # except Exception as e:
-        #     print("Error: " + str(e))
-        #     print("Unable to make manifest for batch " + j + "\n")
-        #     log.write("Batch " + j + " failed: " + str(e) + "\n")
-        # try:
-        #     manifest = socket.readlines()
-        #     socket.close()
-        #     fullDataPaths = []
-        #     for k in manifest:
-        #         partialDataPath = k.split()
-        #         if partialDataPath[1].endswith('.jp2') and partialDataPath[1].count('/') == 4:
-        #             fullDataPaths.append(j+partialDataPath[1]+'\n')
-        #             fullDataPaths.sort()
-        #
-        #     openf.writelines(fullDataPaths)
-        # except Exception as e:
-        #     print("Encountered an error with batch " + j + " : "+ str(e) + "\n")
-        #     log.write("Batch " + j + " failed: " + str(e) + "\n")
     openf.close()
     log.close()
+
+
+
+#This function, when given a begin year and an end year, will search through the full Manifest file
+#and download all images within a year range. For images from a single year, the same year is used for both parameters.
+#The images are downloaded to the following directory structure: data/FullPages/BatchLevel/IssueLevel/PageLevel
+#This function also uses wget in order to download images, this is due to complications we ran into
+#using urllib with the Library of Congress's server.
+def getImages(startYear = 1836, endYear = datetime.now().year):
+    Error404 = []
+    imageCount = 0
+    with open(manifest_file, "r") as masterManifest:
+        for line in masterManifest:
+            lineList = line.split('/')
+            imageYear = int(lineList[9][:4])
+            if imageYear >= int(startYear) and imageYear <= int(endYear):
+                imageCount += 1
+
+    with open(manifest_file, "r") as masterManifest:
+        previousLine = ""
+        pageCount = 1
+        fullCount = 0
+        for line in masterManifest:
+            lineList = line.split('/')
+
+            #used in construction of the image's filename.
+            #ensures consistent naming since each issue of a newspaper shares the same base name
+            if lineList[9] == previousLine:
+                pageCount += 1
+            else:
+                pageCount = 1
+
+            imageYear = int(lineList[9][:4])
+            if imageYear >= int(startYear) and imageYear <= int(endYear):
+                fullCount += 1
+                imageURL = line
+
+                #constructs file and directory names for sorting purposes
+                batchName = lineList[5][6:]
+                snNumber = lineList[7]
+                date = lineList[9][:4]+"-"+lineList[9][4:6]+"-"+lineList[9][6:8]
+                edition = lineList[9][-1:]
+                issueName = snNumber+"_"+date+"_ed-"+edition
+                imageName = issueName+"_seq-"+str(pageCount)+".jp2"
+
+                if not os.path.exists("data/FullPages/"+batchName):
+                    os.makedirs("data/FullPages/"+batchName)
+                if not os.path.exists("data/FullPages/"+batchName+"/"+issueName):
+                    os.makedirs("data/FullPages/"+batchName+"/"+issueName)
+
+
+
+                os.chdir("data/FullPages/"+batchName+"/"+issueName)
+                print(imageURL)
+
+
+                r = requests.get(imageURL, stream=True)
+                if r.status_code == 200:
+                    with open(imageName, 'w') as f:
+                        r.raw.decode_content = True
+                        shutil.copyfileobj(r.raw, f)
+
+                os.chdir("../../../../")
+
+                #
+                # try:
+                #     #print(os.getcwd())
+                #     # os.chdir("data/FullPages/"+batchName+"/"+issueName)
+                #     # print(imageURL)
+                #     # print(requests.get(imageURL).content)
+                #
+                #     # f = open(imageName,'w')
+                #     # f.write(urllib.urlopen(imageURL).read())
+                #     # f.close()
+                #
+                #     # response = urllib2.urlopen(imageURL)
+                #     # with open(imageName, "w") as imageFile:
+                #     #     imageFile.write(response.read())
+                #
+                #     # #Status update on the number of images downloaded
+                #     # sys.stdout.write("\rProcessed Image "+str(fullCount)+"/"+str(imageCount)+"           ")
+                #     # sys.stdout.flush()
+                #     # os.chdir("../../../../")
+                #
+                # except:
+                #     print("HELP!")
+
+
+
+            previousLine = lineList[9]
+    if len(Error404) > 0:
+        print("This files returned a 404 error when trying to download the images.")
+        print(Error404)
+    else:
+        print("All files downloaded with no errors")
 
 
 def usage():
@@ -139,11 +203,11 @@ elif sys.argv[1] == "1":
     print("Preparing to build manifest and get images")
     buildFullManifest()
     getImages(sys.argv[2], sys.argv[3])
-    convertToJpg()
+    # convertToJpg()
 elif sys.argv[1] == "2":
     print("Preparing to get images")
     getImages(sys.argv[2], sys.argv[3])
-    convertToJpg()
+    # convertToJpg()
 elif sys.argv[1] == "3":
     print("Preparing to build manifest")
     buildFullManifest()
